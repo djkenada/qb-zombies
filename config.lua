a4bcb718927d002a6de9eeb3b6294a8440666f87
Brian Roberts <37253827+djkenada@users.noreply.github.com>
2021-12-25 17:54:17 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,25 +1,25 @@
 --[[ Default Config Settings ]]--
 Config = {}
-Config.Debug 					= false						-- Set to true for debuging.
-Config.NoPeds 					= true						-- Set to true for no peds.
-Config.MuteAmbience 			= false						-- Set to true to mute ambience.
-Config.NotHealthRecharge 		= true						-- Set true to not all health auto recharge.
+Config.Debug 						= false						-- Set to true for debuging.
+Config.NoPeds 						= true						-- Set to true for no peds.
+Config.MuteAmbience 					= false						-- Set to true to mute ambience.
+Config.NotHealthRecharge 				= true						-- Set true to not all health auto recharge.
 
 --[[ Zombie Spawn Config Settings ]]--
-Config.SpawnZombie 				= 60 						-- Number of zombies to spawn per player.
-Config.MinSpawnDistance 		= 45 						-- Minimum distance zombies spawn from player.
-Config.MaxSpawnDistance 		= 60 						-- Max distance zombies spawn from player.
-Config.DespawnDistance 			= 90 						-- How far away the zombies spawn.
+Config.SpawnZombie 					= 60 						-- Number of zombies to spawn per player.
+Config.MinSpawnDistance 				= 45 						-- Minimum distance zombies spawn from player.
+Config.MaxSpawnDistance 				= 60 						-- Max distance zombies spawn from player.
+Config.DespawnDistance 					= 90 						-- How far away the zombies spawn.
 
 --[[ Zombie Loot Config Settings ]]--
-Config.ZombieDropLoot 			= true  					-- For zombies to drop loot, set to true.
-Config.RandomChance 			= math.random(1, 100)  		-- Random number for giveaway chance.
-Config.ItemAmount 				= math.random(1,5) 			-- Random number for the ammout of item given.
-Config.AddtionalItem 			= false 					-- Set to true to give extra item on loot.
-Config.AddItem 					= 'metalscrap' 				-- Extra item to give player.
-Config.AddItemAmount 			= math.random(1,3) 			-- Random number for the ammout of extra item given.
-Config.ProbabilityMoneyLoot 	= 33 						-- 33 = 30%
-Config.ProbabilityItemLoot 		= 53 						-- 53-33 = 20%
+Config.ZombieDropLoot 					= true  					-- For zombies to drop loot, set to true.
+Config.RandomChance 					= math.random(1, 100)  				-- Random number for giveaway chance.
+Config.ItemAmount 					= math.random(1,5) 				-- Random number for the ammout of item given.
+Config.AddtionalItem 					= false 					-- Set to true to give extra item on loot.
+Config.AddItem 						= 'metalscrap' 					-- Extra item to give player.
+Config.AddItemAmount 					= math.random(1,3) 				-- Random number for the ammout of extra item given.
+Config.ProbabilityMoneyLoot 				= 33 						-- 33 = 30%
+Config.ProbabilityItemLoot 				= 53 						-- 53-33 = 20%
 
 --[[ Zombie Loot Items Config Settings ]]--
 Config.Items = {
@@ -46,8 +46,8 @@ Config.Items = {
 }
 
 --[[ Zombie Safezone Config Settings ]]--
-Config.SafeZone 				= true	-- Set to true to activate for safezones.
-Config.SafeZoneRadioBlip 		= true	-- Set to true to activate safezone blips.
+Config.SafeZone 					= true						-- Set to true to activate for safezones.
+Config.SafeZoneRadioBlip 				= true						-- Set to true to activate safezone blips.
 
 --[[ Zombie Safezone Location Settings ]]--
 Config.SafeZoneCoords = {
